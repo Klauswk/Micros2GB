00483213fef5e666319c39e28fb21a0d032fc6aa
Klaus Klein <klauswk1@hotmail.com>
2017-11-12 17:12:55 -0200

    Correctly sending data throught serial

--- IHM/app/main.js
+++ IHM/app/main.js
@@ -3,33 +3,63 @@ const { app } = electron;
 const { ipcMain } = require('electron')
 const BrowserWindow = electron.BrowserWindow;
 const path = require('path');
+const fs = require('fs');
+
+function logger(message) {
+  fs.appendFile('log.txt', message + "\n", function (err) {
+    if (err) throw err;
+  });
+}
 
 app.on('ready', () => {
-
   const mainWindow = new BrowserWindow();
   mainWindow.loadURL('file://' + __dirname + '/public/index.html');
   // mainWindow.webContents.openDevTools();
-  const fs = require('fs');
   const path = require('path');
-  var config = JSON.parse(fs.readFileSync( path.join(__dirname, '../config.json')));
+  var config = JSON.parse(fs.readFileSync(path.join(__dirname, '../config.json')));
 
-  const SerialPort = require('serialport');
-  const port = new SerialPort(config.port, { baudRate: config.baudRate });
-  const Readline = SerialPort.parsers.Readline;
-  const Delimiter = SerialPort.parsers.Delimiter;
-  const parser = port.pipe(new Readline({ delimiter: '\987' }));
+  let port = setUpSerial(mainWindow, config);
 
-  // Read data that is available but keep the stream from entering "flowing mode"
-  parser.on('data', function (data) {
-    mainWindow.webContents.send('toggleLed', data.toString('utf8'));
-    console.log('Data:', data.toString('utf8'));
-  });
+  if (port) {
+    ipcMain.on('sendClock', function (event, clock) {
+      port.write(clock.endsWith("\n") ? clock : clock + "\n", writeCallback);
+    });
 
-  // Open errors will be emitted as an error event
-  port.on('error', function (err) {
-    mainWindow.webContents.send('err', err.message);
-    console.log('Error: ', err.message);
-  });
+    ipcMain.on('sendAlarm', function (event, alarm) {
+      port.write(alarm.endsWith("\n") ? alarm : alarm + "\n", writeCallback);
+    });
+
+    ipcMain.on('getTemp', function (event) {
+      port.write("20000000\n", writeCallback);
+    });
+  }
+
+  function writeCallback(err) {
+    if (err) {
+      return logger('Error on write: ', err.message);
+    }
+    logger('message written');
+  };
+
+  function setUpSerial(mainWindow, config) {
+    const SerialPort = require('serialport');
+    const port = new SerialPort(config.port, { baudRate: config.baudRate });
+    const Readline = SerialPort.parsers.Readline;
+    const Delimiter = SerialPort.parsers.Delimiter;
+    const parser = port.pipe(new Readline({ delimiter: '\n' }));
+
+    parser.on('data', function (data) {
+      mainWindow.webContents.send('toggleLed', data.toString('utf8'));
+      logger(data.toString('utf8'));
+    });
+
+    port.on('error', function (err) {
+      mainWindow.webContents.send('err', err.message);
+      logger(err.message);
+    });
+
+    return port;
+  }
 });
 
 app.on('window-all-closed', function () {
--- IHM/app/public/js/app.js
+++ IHM/app/public/js/app.js
@@ -4,10 +4,50 @@ const fs = require('fs');
 
 const { ipcRenderer } = require('electron')
 
-angular.module('hminode').controller('AppController', function ($scope) {
+angular.module('hminode').controller('AppController', function ($scope, $interval) {
     var vm = this;
     vm.messages = [];
     vm.clearLog = clearLog;
+    vm.sendClock = sendClock;
+    vm.getAlarm = getAlarm;
+    vm.getSystemTime = getSystemTime;
+    vm.getTemperature = getTemperature;
+    vm.sendAlarm = sendAlarm;
+    vm.systemTime = new Date();
+    vm.configedAlarm = {};
+    vm.alarm = {};
+
+    function sendAlarm(alarm) {
+        vm.configedAlarm.time = alarm.time;
+        vm.configedAlarm.temperature = alarm.temperature;
+        ipcRenderer.send('sendAlarm',"31"+( alarm.temperature < 10 ? "0"+alarm.temperature : alarm.temperature )+ (alarm.time.getHours() < 10 ? "0" + alarm.time.getHours() : alarm.time.getHours())+""+(alarm.time.getMinutes() < 10 ? "0"+alarm.time.getMinutes() : alarm.time.getMinutes()));
+        vm.alarm = {};
+    }
+
+    function getAlarm() {
+        if (!vm.configedAlarm.temperature || !vm.configedAlarm.time) {
+            return "Alarme ainda não definido";
+        }
+
+        return vm.configedAlarm.temperature + "º ás " + (vm.configedAlarm.time.getHours() < 10 ? "0"+vm.configedAlarm.time.getHours() : vm.configedAlarm.time.getHours()) + ":" + (vm.configedAlarm.time.getMinutes() < 10 ? "0" + vm.configedAlarm.time.getMinutes() : vm.configedAlarm.time.getMinutes());
+    }
+
+    function sendClock(time) {
+        vm.systemTime = time;
+        ipcRenderer.send('sendClock',"11"+(time.getHours() < 10 ? "0" + time.getHours() : time.getHours())+""+(time.getMinutes() < 10 ? "0"+time.getMinutes() : time.getMinutes())+"00");
+    }
+
+    function getTemperature() {
+        ipcRenderer.send('getTemp');
+    }
+
+    function getSystemTime() {
+        return vm.systemTime;
+    }
+
+    $interval(function () {
+        vm.systemTime = new Date(vm.systemTime.getTime() + 1000);
+    }, 1000);
 
     function clearLog() {
         vm.messages = [];
